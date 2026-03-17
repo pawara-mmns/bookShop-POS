f60e0bd155570e1fe28c263484b9e7b42ef4d956
pawara-mmns <pawarasamarawickrama@gmail.com>
2026-03-17 21:12:54 +0530

    Update AssemblyInfo to include detailed informational version

--- obj/Debug/net8.0/bookShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/bookShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b24153e2936bb0dbc6a20ba7183e3913f8a22d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
