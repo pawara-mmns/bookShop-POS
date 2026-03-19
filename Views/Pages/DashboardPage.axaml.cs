e04b623a32a5e9ba48db403b10def725d1418721
pawara-mmns <pawarasamarawickrama@gmail.com>
2026-03-19 10:42:34 +0530

    Add: create DashboardPage with layout and components for dashboard overview

---
+++ Views/Pages/DashboardPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace bookShop.Views.Pages;
+
+public partial class DashboardPage : UserControl
+{
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+}
