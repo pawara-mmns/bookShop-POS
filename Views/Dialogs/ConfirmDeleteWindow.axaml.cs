166ac6b654c9f16e6ee3d9220aaed2074f1f27d2
pawara-mmns <pawarasamarawickrama@gmail.com>
2026-03-19 16:30:42 +0530

    Refactor: update ConfirmDeleteWindow to use named parameters for improved clarity

--- Views/Dialogs/ConfirmDeleteWindow.axaml.cs
+++ Views/Dialogs/ConfirmDeleteWindow.axaml.cs
@@ -5,13 +5,21 @@ namespace bookShop.Views.Dialogs;
 
 public partial class ConfirmDeleteWindow : Window
 {
-    public ConfirmDeleteWindow() : this("Are you sure you want to delete this customer?")
+    public ConfirmDeleteWindow() : this("Delete Customer", "Are you sure you want to delete this customer?")
     {
     }
 
-    public ConfirmDeleteWindow(string message)
+    public ConfirmDeleteWindow(string message) : this("Delete Customer", message)
+    {
+    }
+
+    public ConfirmDeleteWindow(string heading, string message)
     {
         InitializeComponent();
+
+        var headingText = this.FindControl<TextBlock>("HeadingText");
+        if (headingText is not null)
+            headingText.Text = heading;
 
         var msg = this.FindControl<TextBlock>("MessageText");
         if (msg is not null)
