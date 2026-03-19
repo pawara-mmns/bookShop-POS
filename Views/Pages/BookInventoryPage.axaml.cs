fc112870a662d9d4d72dbb0259d2709427a79048
pawara-mmns <pawarasamarawickrama@gmail.com>
2026-03-19 12:05:17 +0530

    Add: create BookInventoryPage and PosBillingPage with layout and placeholder text

---
+++ Views/Pages/BookInventoryPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace bookShop.Views.Pages;
+
+public partial class BookInventoryPage : UserControl
+{
+    public BookInventoryPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/Pages/PosBillingPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace bookShop.Views.Pages;
+
+public partial class PosBillingPage : UserControl
+{
+    public PosBillingPage()
+    {
+        InitializeComponent();
+    }
+}
